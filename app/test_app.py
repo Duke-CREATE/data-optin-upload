20b0d5f4b108fb5cf17d8640cc667279a0823069
Mike <michaelkeohane01@gmail.com>
2023-12-15 22:23:42 -0500

    updated test

--- app/test_app.py
+++ app/test_app.py
@@ -1,7 +1,8 @@
 # create dummy test function
-import app
+from app import create_app
 from flask import Flask
 def test_app():
+    app = create_app()
     # write test to check if app is running and html returned from endpoint
     with app.test_client() as test_client:
         response = test_client.get('/')
