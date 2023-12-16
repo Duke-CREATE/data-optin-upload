c22e85db326fadaefd60292334b11e2fdbc72461
Mike <michaelkeohane01@gmail.com>
2023-12-15 22:03:29 -0500

    updated test

--- app/test_app.py
+++ app/test_app.py
@@ -2,5 +2,17 @@
 import app
 from flask import Flask
 def test_app():
-    # assert that the app is a flask instance
-    assert isinstance(app, Flask)
+    # write test to check if app is running and html returned from endpoint
+    with app.test_client() as test_client:
+        response = test_client.get('/')
+        assert response.status_code == 200
+        assert b'File Upload Form' in response.data
+        assert b'Name' in response.data
+        assert b'Subject' in response.data
+        assert b'File' in response.data
+        assert b'Upload' in response.data
+        assert b'No file part. Please try again.' not in response.data
+        assert b'No selected file. Please try again.' not in response.data
+        assert b'Thank you for contributing' not in response.data
+        assert b'Something went wrong. Please try again.' not in response.data
+
