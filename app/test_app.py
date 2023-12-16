9aa43b18770fe414b0203cd3dd3f1a0c5ecc6345
Mike <michaelkeohane01@gmail.com>
2023-12-15 22:33:31 -0500

    updated test

--- app/test_app.py
+++ app/test_app.py
@@ -7,13 +7,7 @@ def test_app():
     with app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
-        assert b'File Upload Form' in response.data
         assert b'Name' in response.data
         assert b'Subject' in response.data
         assert b'File' in response.data
         assert b'Upload' in response.data
-        assert b'No file part. Please try again.' not in response.data
-        assert b'No selected file. Please try again.' not in response.data
-        assert b'Thank you for contributing' not in response.data
-        assert b'Something went wrong. Please try again.' not in response.data
-
